ea27d08b754ededf85ab056e57cf764cb345ec0e
Nicolau NP <mr1Normaliii@gmail.com>
2023-03-04 23:06:40 +0000

    update

--- database/seeders/UsuarioSeeder.php
+++ database/seeders/UsuarioSeeder.php
@@ -15,7 +15,7 @@ class UsuarioSeeder extends Seeder
             [
                 'id_instituicao' => null,
                 'id_pessoa' => 1,
-                'name' => 'admin.inscritor',
+                'name' => 'manager.inscritor',
                 'password' => Hash::make('welwitchia2023'),
                 'nivel_acesso' => 'manager',
                 'estado' => 'on',
@@ -23,7 +23,7 @@ class UsuarioSeeder extends Seeder
             [
                 'id_instituicao' => 1,
                 'id_pessoa' => 2,
-                'name' => 'liceu.welwitchia0',
+                'name' => 'admin.inscritor',
                 'password' => Hash::make('welwitchia2023'),
                 'nivel_acesso' => 'admin',
                 'estado' => 'on',
@@ -31,7 +31,7 @@ class UsuarioSeeder extends Seeder
             [
                 'id_instituicao' => 1,
                 'id_pessoa' => 3,
-                'name' => 'liceu.welwitchia1',
+                'name' => 'user.inscritor',
                 'password' => Hash::make('welwitchia2023'),
                 'nivel_acesso' => 'user',
                 'estado' => 'on',
