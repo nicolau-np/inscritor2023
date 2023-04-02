e6bab8fc309458dd50d29d5a9d43a384c638ddd0
Nicolau NP <mr1Normaliii@gmail.com>
2023-04-02 12:04:04 +0100

    update

--- app/Http/Controllers/api/ListaController.php
+++ app/Http/Controllers/api/ListaController.php
@@ -5,19 +5,11 @@ namespace App\Http\Controllers\api;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 
+
 class ListaController extends Controller
 {
-    public function index($estado)
+    public function index()
     {
 
-        $estudantes = null;
-
-        if ($estado == "admitidos") {
-
-        } elseif ($estado == "nadmitidos") {
-            
-        }
-
-        return $estudantes;
     }
 }
