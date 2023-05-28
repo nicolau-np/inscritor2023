07f36a58ac50b398ec9686fd456e6f3aaf7ff5d1
nicolau-np <mr1Normaliii@gmail.com>
2023-05-28 12:20:27 +0100

    update

--- resources/views/client/consultar.blade.php
+++ resources/views/client/consultar.blade.php
@@ -32,19 +32,26 @@
         </div>
         @if($estudante!="no")
             <div class="col-md-5">
-                <div class="perfil" style="padding:8px;">
-                    <span style="font-weight:bold;">Nº de Inscrição: {{ $inscricao }}</span><br/>
-                    <span style="font-weight:bold;">Instituição:</span> {{ $estudante->instituicaos->instituicao }}<br/>
-                    <span style="font-weight:bold;">Nome Completo:</span> {{ $estudante->pessoas->nome ?? null }}<br/>
-                    <span style="font-weight:bold;">Nº de Bilhete:</span> {{ $estudante->pessoas->bilhete ?? null }}<br/>
-                    <span style="font-weight:bold;">Curso:</span> {{ $estudante->cursos->curso }}<br/>
+                <div class="card mt-4">
+                    <div class="card-body">
+                        <div class="perfil" style="padding:8px;">
+                            <span style="font-weight:bold;">Nº de Inscrição: {{ $inscricao }}</span><br/>
+                            <span style="font-weight:bold;">Instituição:</span> {{ $estudante->instituicaos->instituicao }}<br/>
+                            <span style="font-weight:bold;">Nome Completo:</span> {{ $estudante->pessoas->nome ?? null }}<br/>
+                            <span style="font-weight:bold;">Nº de Bilhete:</span> {{ $estudante->pessoas->bilhete ?? null }}<br/>
+                            <span style="font-weight:bold;">Curso:</span> {{ $estudante->cursos->curso }}<br/>
 
+                        </div>
+                    </div>
+                    <div class="card-footer">
+                        @if($estudante->data_nascimento >= $classificador->data_inicio && $estudante->data_nascimento <= $classificador->data_fim)
+                            <div class="alert alert-success">Admitido</div>
+                        @elseif($estudante->data_nascimento <= $classificador->data_inicio || $estudante->data_nascimento >= $classificador->data_fim)
+                            <div class="alert alert-danger">Não Admitido</div>
+                        @endif
+                    </div>
                 </div>
-                @if($estudante->data_nascimento >= $classificador->data_inicio && $estudante->data_nascimento <= $classificador->data_fim)
-                    <div class="alert alert-success">Admitido</div>
-                @elseif($estudante->data_nascimento <= $classificador->data_inicio || $estudante->data_nascimento >= $classificador->data_fim)
-                    <div class="alert alert-danger">Não Admitido</div>
-                @endif
+
             </div>
         @endif
       </div>
